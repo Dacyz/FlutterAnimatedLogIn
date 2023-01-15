ea1d25907711d7c4bbf7343fbcbf8858d981a665
Dacyz <dacygaming@gmail.com>
2023-01-15 13:55:56 -0500

    UI> Added Scale Animation to ImageContainer

--- lib/presentation/components/image_container.dart
+++ lib/presentation/components/image_container.dart
@@ -12,38 +12,49 @@ class ImageContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 120,
-      height: 120,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.3),
-            spreadRadius: 2,
-            blurRadius: 5,
-            offset: const Offset(0, 3),
-          ),
-        ],
+    return TweenAnimationBuilder(
+      builder: (_, value, child) => Opacity(
+        opacity: value,
+        child: Transform.scale(
+          scale: 1 * value,
+          child: Transform.scale(scale: 1, child: child),
+        ),
       ),
-      child: imageSrc != null && imageSrc != ''
-          ? CircleAvatar(
-              backgroundColor: Colors.white,
-              radius: 45,
-              child: ClipOval(
-                child: Image.file(
-                  File(imageSrc ?? ''),
-                  fit: BoxFit.cover,
-                  width: 120,
-                  height: 120,
-                ),
-              ),
-            )
-          : const Icon(
-              Icons.person,
-              size: 42,
+      duration: const Duration(milliseconds: 500),
+      tween: Tween(begin: 0.7, end: 1.0),
+      child: Container(
+        width: 120,
+        height: 120,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: const Offset(0, 3),
             ),
+          ],
+        ),
+        child: imageSrc != null && imageSrc != ''
+            ? CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 45,
+                child: ClipOval(
+                  child: Image.file(
+                    File(imageSrc ?? ''),
+                    fit: BoxFit.cover,
+                    width: 120,
+                    height: 120,
+                  ),
+                ),
+              )
+            : const Icon(
+                Icons.person,
+                size: 42,
+              ),
+      ),
     );
   }
 }
